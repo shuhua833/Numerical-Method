da5fa16503adbc3d2f141159e3f7c56b6ccd272e
Shuhua Shrestha <71085603+shuhua833@users.noreply.github.com>
2022-01-23 07:35:53 +0545

    Update least_sqaure.c

--- least_sqaure.c
+++ least_sqaure.c
@@ -2,30 +2,30 @@
 #include<conio.h>
 int main()
 {
-int i,n;
-float x[100],y[100],Tx=0,Tx2=0,Ty=0,Tyx=0,b,a;
-printf("Enter n: ");
-scanf("%d",&n);
-
-printf("Enter the values:\n");
-for(i=0;i<n;i++)
-{
-printf("Enter x[%d] and y[%d]: ",i,i);
-scanf("%f%f",&x[i],&y[i]);
-}
-
-for(i=0;i<n;i++)
-{
-Tx=Tx+x[i];
-Tx2=Tx2+x[i]*x[i];
-Ty=Ty+y[i];
-Tyx=Tyx+y[i]*x[i];
-}
-
-b=((n*Tyx)-(Tx*Ty))/((n*Tx2)-(Tx*Tx));
-a=(Ty/n)-b*(Tx/n);
-
-printf("\ny = %f + %fx",a,b);
-
-getch();
+	
+    int i,n;
+    float x[100],y[100],Tx=0,Tx2=0,Ty=0,Tyx=0,b,a;
+    printf("Enter n: ");
+    scanf("%d",&n);
+    printf("Enter the values:\n");
+    for(i=0;i<n;i++)
+    {
+    	
+        printf("Enter x[%d] and y[%d]: ",i,i);
+        scanf("%f%f",&x[i],&y[i]);
+    }
+    
+    for(i=0;i<n;i++)
+    {
+    	
+        Tx=Tx+x[i];
+        Tx2=Tx2+x[i]*x[i];
+        Ty=Ty+y[i];
+        Tyx=Tyx+y[i]*x[i];
+   }
+   
+    b=((n*Tyx)-(Tx*Ty))/((n*Tx2)-(Tx*Tx));
+    a=(Ty/n)-b*(Tx/n);
+    printf("\ny = %f + %fx",a,b);
+    getch();
 }
